c2cdf1230b140ae11a293077214feb3bce984e21
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-03 12:24:59 -0700

    Create findBy users method

--- models/Users.js
+++ models/Users.js
@@ -1,9 +1,14 @@
 const db = require('../database/dbConfig');
 
 module.exports = {
-	find
+	find,
+	findBy
 };
 
 function find() {
 	return db('users');
+}
+
+function findBy(filter) {
+	return db('users').where(filter);
 }
