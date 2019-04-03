2a11e8e44adafc2c1ca0d1119082a046a33b2c5f
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-03 12:50:47 -0700

    Handle login user endpoint

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../models/Users');
 
+// register user
 router.post('/register', async (req, res) => {
 	try {
 		let user = req.body;
@@ -19,6 +21,41 @@ router.post('/register', async (req, res) => {
 	}
 });
 
-router.post('/login', async (req, res) => {});
+// login user
+router.post('/login', async (req, res) => {
+	try {
+		let { username, password } = req.body;
+
+		const user = await Users.findBy({ username }).first();
+
+		if (user && bcrypt.compareSync(password, user.password)) {
+			const token = generateToken(user);
+			return res.status(200).json(token);
+		}
+
+		return res
+			.status(401)
+			.json({ message: 'Sorry, incorrect username or password' });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: 'Sorry, but there was an error while trying to log in '
+		});
+	}
+});
+
+function generateToken(user) {
+	const payload = {
+		subject: user.id,
+		username: user.username,
+		department: user.department
+	};
+
+	const options = {
+		expiresIn: '1d'
+	};
+
+	return jwt.sign(payload, process.env.JWT_SECRET, options);
+}
 
 module.exports = router;
