306fe7f7f768df9f87a0c19206c54aa226ef2b27
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-03 12:20:44 -0700

    Configure database

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require('express');
 
 const middleware = require('./middleware/config');
 
+// controllers
 const userController = require('./controllers/user');
 
 const server = express();
 
+// middleware
 server.use(middleware(server));
 
+// route handlers
 server.use('/api/', userController);
 
 const port = process.env.PORT;
