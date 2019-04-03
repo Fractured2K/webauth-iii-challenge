e71ddf7b536aa0bf8bf4c63188834eee9362eab5
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-03 12:06:08 -0700

    Create user controller

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 
+//  Controllers
+const userController = require('./controllers/user');
+
 const server = express();
+
+// Route handling
+server.use('/api/', userController);
 
 const port = process.env.PORT;
 server.listen(port, () => console.log(`Server now listening on port ${port}`));
