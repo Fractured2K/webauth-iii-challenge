f81c2dc669bd1b94eb0c040ec8085015a930733e
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-04 14:04:19 -0700

    Create onSubmit method for login component

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Login extends Component {
 	state = {
@@ -19,6 +20,23 @@ class Login extends Component {
 		this.setState({
 			[e.target.name]: e.target.value
 		});
+	};
+
+	onSubmit = e => {
+		e.preventDefault();
+
+		axios
+			.post('http://localhost:5000/api/login', this.state)
+			.then(res => {
+				localStorage.setItem('token', res.data);
+
+				this.setState({
+					username: '',
+					password: '',
+					department: ''
+				});
+			})
+			.catch(err => console.log(err));
 	};
 }
 
