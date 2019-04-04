2207646c8083d9c1a56d7967eb9d89343deb83d7
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-04 14:06:36 -0700

    Create login route

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import { Route, NavLink } from 'react-router-dom';
 import './App.css';
 
 import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Route path="/register" component={Register} />
+				<Route path="/login" component={Login} />
 			</div>
 		);
 	}
