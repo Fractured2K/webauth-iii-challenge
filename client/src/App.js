366b6ab87557af5561ae0615096089be3b5d1db0
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-04 13:39:42 -0700

    Create register route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+
 import './App.css';
+
+import Register from './components/Auth/Register';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>Hello</h1>
+				<Route path="/register" component={Register} />
 			</div>
 		);
 	}
