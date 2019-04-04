a35fa7b32296a14c9c41801933ce5351dc6720a4
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-04 13:29:54 -0700

    Create handleChanges method for reigster component

--- client/src/components/Authentication/Register.js
+++ client/src/components/Authentication/Register.js
@@ -14,6 +14,12 @@ class Register extends Component {
 			</div>
 		);
 	}
+
+	handleChanges = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
 }
 
 export default Register;
