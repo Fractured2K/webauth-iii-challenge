7f06e06d172216956c822be93069c01f0bb0ae1e
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-04 13:32:43 -0700

    Create register component form

--- client/src/components/Authentication/Register.js
+++ client/src/components/Authentication/Register.js
@@ -10,7 +10,29 @@ class Register extends Component {
 	render() {
 		return (
 			<div>
-				<h1>Register</h1>
+				<form>
+					<input
+						type="text"
+						name="username"
+						placeholder="username"
+						value={this.state.username}
+						onChange={this.handleChanges}
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="password"
+						value={this.state.password}
+						onChange={this.handleChanges}
+					/>
+					<input
+						type="text"
+						name="department"
+						placeholder="department"
+						value={this.state.department}
+						onChange={this.handleChanges}
+					/>
+				</form>
 			</div>
 		);
 	}
